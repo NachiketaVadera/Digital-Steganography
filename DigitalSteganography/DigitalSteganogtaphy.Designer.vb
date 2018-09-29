edabc54a2fa53d891ee3a751f6d9c5c5e455b3b0
Nachiketa Vadera <personal.nachiketa@gmail.com>
2018-09-30 03:34:29 +0530

    Add project files.

---
+++ DigitalSteganography/DigitalSteganogtaphy.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class winMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtDataInput = New System.Windows.Forms.TextBox()
+        Me.btnChooseImage = New System.Windows.Forms.Button()
+        Me.btnExecute = New System.Windows.Forms.Button()
+        Me.pbxImage = New System.Windows.Forms.PictureBox()
+        CType(Me.pbxImage, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtDataInput
+        '
+        Me.txtDataInput.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtDataInput.Location = New System.Drawing.Point(20, 18)
+        Me.txtDataInput.Margin = New System.Windows.Forms.Padding(5)
+        Me.txtDataInput.Multiline = True
+        Me.txtDataInput.Name = "txtDataInput"
+        Me.txtDataInput.Size = New System.Drawing.Size(1008, 66)
+        Me.txtDataInput.TabIndex = 0
+        '
+        'btnChooseImage
+        '
+        Me.btnChooseImage.Location = New System.Drawing.Point(20, 92)
+        Me.btnChooseImage.Name = "btnChooseImage"
+        Me.btnChooseImage.Size = New System.Drawing.Size(179, 35)
+        Me.btnChooseImage.TabIndex = 2
+        Me.btnChooseImage.Text = "Choose an Image"
+        Me.btnChooseImage.UseVisualStyleBackColor = True
+        '
+        'btnExecute
+        '
+        Me.btnExecute.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnExecute.Location = New System.Drawing.Point(946, 92)
+        Me.btnExecute.Name = "btnExecute"
+        Me.btnExecute.Size = New System.Drawing.Size(84, 35)
+        Me.btnExecute.TabIndex = 3
+        Me.btnExecute.Text = "Process"
+        Me.btnExecute.UseVisualStyleBackColor = True
+        '
+        'pbxImage
+        '
+        Me.pbxImage.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pbxImage.Location = New System.Drawing.Point(20, 133)
+        Me.pbxImage.Name = "pbxImage"
+        Me.pbxImage.Size = New System.Drawing.Size(1010, 414)
+        Me.pbxImage.TabIndex = 4
+        Me.pbxImage.TabStop = False
+        '
+        'winMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1042, 559)
+        Me.Controls.Add(Me.pbxImage)
+        Me.Controls.Add(Me.btnExecute)
+        Me.Controls.Add(Me.btnChooseImage)
+        Me.Controls.Add(Me.txtDataInput)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.Margin = New System.Windows.Forms.Padding(5)
+        Me.Name = "winMain"
+        Me.Text = "Digital Steganography"
+        CType(Me.pbxImage, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtDataInput As TextBox
+    Friend WithEvents btnChooseImage As Button
+    Friend WithEvents btnExecute As Button
+    Friend WithEvents pbxImage As PictureBox
+End Class
---
+++ DigitalSteganography/DigitalSteganogtaphy.vb
@@ -0,0 +1,83 @@
+﻿Public Class winMain
+
+    Shared count As Integer
+
+    Private Sub BtnExecute_Click(sender As Object, e As EventArgs) Handles btnExecute.Click
+        If DataInImage() Then
+            Dim saveFileDialog As SaveFileDialog = New SaveFileDialog()
+            saveFileDialog.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif"
+            saveFileDialog.Title = "Save Image As"
+            saveFileDialog.ShowDialog()
+
+            If Not saveFileDialog.FileName.Equals("") Then
+                Dim fileStream As IO.FileStream = CType(saveFileDialog.OpenFile, System.IO.FileStream)
+                Select Case (saveFileDialog.FilterIndex)
+                    Case 1
+                        pbxImage.Image.Save(fileStream, Imaging.ImageFormat.Jpeg)
+                    Case 2
+                        pbxImage.Image.Save(fileStream, Imaging.ImageFormat.Bmp)
+                    Case 3
+                        pbxImage.Image.Save(fileStream, Imaging.ImageFormat.Gif)
+                End Select
+            End If
+
+        Else
+            MsgBox("Sorry, there was an error while processing the image")
+        End If
+    End Sub
+
+    Private Sub BtnChooseImage_Click(sender As Object, e As EventArgs) Handles btnChooseImage.Click
+        Dim fileDialog As OpenFileDialog = New OpenFileDialog()
+        Dim strFilePath As String
+
+        fileDialog.Title = "Choose an Image"
+        fileDialog.Filter = "JPEGs|*.jpg|GIFs|*.gif|Bitmaps|*.bmp|AllFiles|*.*"
+        fileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+        fileDialog.RestoreDirectory = True
+
+        If fileDialog.ShowDialog = DialogResult.OK Then
+            strFilePath = fileDialog.FileName
+            pbxImage.Image = Image.FromFile(strFilePath)
+            pbxImage.SizeMode = PictureBoxSizeMode.Zoom
+        End If
+
+    End Sub
+
+    Private Function DataInImage() As Boolean
+        Dim textToEmbedded As String = txtDataInput.Text
+        Dim listOfBits As New List(Of Integer)
+        Dim data(textToEmbedded.Length) As String
+        Dim bmpImage As New Bitmap(pbxImage.Image)
+
+        listOfBits.Clear()
+
+        count = 0
+
+        For i = 0 To textToEmbedded.Length
+            If i <> textToEmbedded.Length Then
+                data(i) = Convert.ToString(Asc(textToEmbedded.Substring(i, 1)), 2).PadLeft(8, "0")
+            Else
+                data(i) = "00000000"
+            End If
+        Next
+
+        For i = 0 To data.Length - 1
+            For j = 0 To data(i).Length - 1
+                listOfBits.Add(data(i).Substring(j, 1))
+            Next
+        Next
+
+        For x = 0 To bmpImage.Width - 1
+            For y = 0 To bmpImage.Height - 1
+                Dim color As Color = bmpImage.GetPixel(x, y)
+                Dim red = Convert.ToString(color.R, 2).PadLeft(8, "0")
+                Dim stegRed = red.Substring(0, 7) & listOfBits(count)
+                count = count + 1
+                bmpImage.SetPixel(x, y, Color.FromArgb(Convert.ToInt16(stegRed, 2), color.G, color.B))
+                Return True
+            Next
+        Next
+        Return False
+    End Function
+
+End Class
